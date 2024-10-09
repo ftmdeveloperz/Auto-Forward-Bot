d029982e07965aef0ee8e89db5453c221a3df0e6
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-10-09 17:06:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from os import getenv
 class Config(object):
       API_HASH = getenv("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
       API_ID = int(getenv("API_ID", "28776072"))
-      AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "7332604369:AAGOEgjVZeXacMoyOlJBxZvtoX41wc8zzCI")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002178995654:-1002369365561").replace("\n", " ").split(' '))
+      AS_COPY = True if getenv("AS_COPY", False) == "`{file_name}`" else True
+      BOT_TOKEN = getenv("BOT_TOKEN", "7291479898:AAEOcnAWa15wcBoVY2pH13PRmdPl3r6TSco")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002261985371:-1002487385156").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
